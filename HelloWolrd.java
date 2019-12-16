8c25c5e44cc861f084f985de252f77eacc469617
Oroken <34053443+Oroken@users.noreply.github.com>
2019-12-16 23:44:10 +0700

    Add files via upload

---
+++ HelloWolrd.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellowolrd;
+
+/**
+ *
+ * @author Pham Minh Nhat
+ */
+public class HelloWolrd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void sayHiToWorld() {
+        System.out.println("Hello World!\n");
+        System.out.println("It's me! Nathan, Nice to meet u");
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        sayHiToWorld();
+    }
+    
+}
